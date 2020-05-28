3c24b7261b89c340ebf2628e381d44893e4d1f0f
Caerwyn Jones <caerwynj@gmail.com>
2020-05-28 15:41:04 +0000

    rewrote to set baseline.

--- index.js
+++ index.js
@@ -1,42 +1,88 @@
 'use strict';
 
-function State() {
-  let reward = 0;
-  let data = Array(10).fill(0.0);
-}
+const mkState = function() {
+  let data = Array.from({length: 10}, d => 0.0);
 
-function Agent() {
-
-}
-
-Agent.prototype = {
-  step: function(state){
-    let action = ["a"];
-    return action;
-  }
-}
-
-function Env() {
-    let population = [];
-    let state = new State();
-}
-
-Env.prototype = {
-  step: function() {
-    let actionList = this.population
-      .map(p => p.step(this.state))
-      .reduce((acc, val) => acc.concat(val), []);
-    return actionList;
+  let getObservable = function () {
+    return data;
   }
 
-  simulate: function(n) {
-    for (let i = 0; i < n; i++){
-      let a = env.step();
-      console.log(a);
+  let update = function (actionList) {
+    actionList.forEach(a => {
+      data.push(a);
+      data.shift();
+    })
+  }
+
+  let reward = function () {
+
+  }
+
+  return {getObservable, update, reward};
+}
+
+const mkActionSpace = function () {
+
+  return {};
+}
+
+const mkAgent = function () {
+  let epsilon = 0.1;
+
+  let action =  function(state){
+    let r = Math.random();
+    let a = 0;
+    if (r < epsilon) {
+      a = Math.floor(Math.random() * 10);
+    } else {
+      a = state.reduce((acc, val) => val + acc, 0) / state.length;
     }
+    return a;
+  }
+
+  return {action};
+}
+
+const mkEnv = function () {
+  let population = [];
+  let state = mkState();
+  let actionSpace = mkActionSpace();
+
+  let step =  function() {
+    let obs = state.getObservable();
+    let actionList = population
+      .map(p => p.action(obs))
+      .reduce((acc, val) => acc.concat(val), []);
+
+    state.update(actionList);
+    obs = state.getObservable();
+    let reward = 0;
+    let done = false;
+
+    return {state: obs, reward, done};
+  }
+
+  let addAgent = function (agent) {
+    population.push(agent);
+  }
+
+  let reset = function() {
+  }
+
+  return {step, reset, addAgent};
+}
+  
+const simulate =  function(n) {
+  let env = mkEnv()
+  for (let i = 0; i < 4; i++) {
+    env.addAgent(mkAgent());
+  }
+  let state, reward, done;
+  state = env.reset();
+  for (let i = 0; i < n && !done; i++){
+    ({state, reward, done} = env.step());
+    console.log(state);
   }
 }
 
-let env = new Env()
-env.population = [new Agent(), new Agent()];
-env.simulate(10);
+simulate(10);
